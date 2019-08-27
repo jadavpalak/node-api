fecb784fd10335214af7097e92594231219b9a85
palak <palakjadav.1996@gmail.com>
2019-08-27 20:04:12 +0530

    filter added

--- blog-routing.js
+++ blog-routing.js
@@ -32,22 +32,39 @@ blogRouting.route('/all').get(function (req, res) {
     if(req.query.sortOrder=='desc'){
         order = -1;
     }
+    let filter = req.query.search;
+    var filterQuery = {}; 
     var sort = { name: order };
     var query = {};
     query.skip = limit * (page - 1);
     query.limit = limit;
     query.sort = sort;
-    Blog.countDocuments({}, function (err, totalCount) {
+    var filterQuery = {};
+    if (filter != '') {
+        filter = JSON.parse(filter);
+        if ('name' in filter) {
+            console.log(filter.name.value);
+            filterQuery.name = new RegExp(filter.name.value);
+        }
+        if ('email' in filter) {
+            filterQuery.email = new RegExp(filter.email.value);
+        }
+        if ('description' in filter) {
+            filterQuery.description = new RegExp(filter.description.value);
+        }
+    };
+    Blog.countDocuments(filterQuery, function (err, totalCount) {
         if (err) {
             response = { "error": true, "message": "Error fetching data" }
         }
-        Blog.find({}, {}, query, function (err, data) {
-        if (err) {
-            response = { "error": true, "message": "Error fetching data" };
-        } else {
-            response = { items: data, total: totalCount };
-        }
-        res.json(response);
+        Blog.find(filterQuery, {}, query, function (err, data) {
+            if (err) {
+                console.log(err);
+                response = { "error": true, "message": "Error fetching data" };
+            } else {
+                response = { items: data, total: totalCount };
+            }
+            res.json(response);
         })
     });
 });
